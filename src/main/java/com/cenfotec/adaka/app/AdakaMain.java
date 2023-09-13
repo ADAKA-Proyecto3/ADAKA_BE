de62de30a72cda19d45d7f7b7d9b7be0aa66b4fb
kristati <ksegurag@ucenfotec.ac.cr>
2023-09-12 22:59:42 -0600

    First commit

---
+++ src/main/java/com/cenfotec/adaka/app/AdakaMain.java
@@ -0,0 +1,12 @@
+package com.cenfotec.adaka.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdakaMain {
+    public static void main(String[] args) {
+
+        SpringApplication.run(AdakaMain.class, args);
+    }
+}
---
+++ src/test/java/com/cenfotec/adaka/app/AdakaMainTest.java
@@ -0,0 +1,13 @@
+package com.cenfotec.adaka.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdakaMainTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
