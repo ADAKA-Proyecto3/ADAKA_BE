5950e7b26339581ce918da180d2e4f9e7d7969c5
Alberto Solano <70608100+AlbertoSolanoV@users.noreply.github.com>
2023-11-01 13:47:45 -0600

    Cambios de mejora
    boolean existsMedicalCenterByNameAndUserId (String name, int userId);
        boolean existsMedicalCenterByIdAndRoomsIsNotEmpty(int id);

--- src/main/java/com/cenfotec/adaka/app/service/impl/MedicalCenterImpl.java
+++ src/main/java/com/cenfotec/adaka/app/service/impl/MedicalCenterImpl.java
@@ -43,13 +43,16 @@ public class MedicalCenterImpl implements MedicalCenterService {
     public MedicalCenter saveMedicalCenter(MedicalCenter medicalCenter, int id) {
         User user = userRepository.findById(id).orElseThrow(() -> new InvalidMedicalCenterException("Not user with ID"));
 
+        if (medicalCenterRepository.existsMedicalCenterByNameAndUserId(medicalCenter.getName(), id)) {
+            throw new InvalidMedicalCenterException("Validation errors: A medical center with that name already exists.");
+        }
         // Realizar validación detallada
         List<String> validationErrors = validateMedicalCenter(medicalCenter, false);
 
         if (!validationErrors.isEmpty()) {
             throw new InvalidMedicalCenterException("Validation errors: " + String.join(", ", validationErrors));
         }
-        if(!validateUserMedicalCenter(user)){
+        if (!validateUserMedicalCenter(user)) {
             throw new InvalidMedicalCenterException("Maximum number of medical centers according to the user's plan");
         }
         // Guardar el centro médico en la base de datos
@@ -60,6 +63,7 @@ public class MedicalCenterImpl implements MedicalCenterService {
 
         return newMedicalCenter;
     }
+
     @Override
     public MedicalCenter updateMedicalCenter(int id, MedicalCenter newMedicalCenter) {
 
@@ -91,16 +95,16 @@ public class MedicalCenterImpl implements MedicalCenterService {
 
     @Override
     public void deleteMedicalCenter(int id) {
-        try {
 
-            MedicalCenter medicalCenter = getMedicalCenterById(id);
-            if ((medicalCenter.getRooms() == null) || medicalCenter.getRooms().isEmpty())
-                medicalCenterRepository.deleteById(id);
-            else
-                throw new InvalidMedicalCenterException("You must remove the rooms associated with this medical center.");
-        } catch (Exception ex) {
-            throw new InvalidMedicalCenterException("Deleting medical center", ex);
+
+        boolean hasRooms = medicalCenterRepository.existsMedicalCenterByIdAndRoomsIsNotEmpty(id);
+
+        if (hasRooms) {
+            throw new InvalidMedicalCenterException("You must remove the rooms associated with this medical center.");
+        } else {
+            medicalCenterRepository.deleteById(id);
         }
+
     }
 
 
@@ -120,7 +124,7 @@ public class MedicalCenterImpl implements MedicalCenterService {
         if (medicalCenter.getEmail().isEmpty()) {
             validate.add("Email");
         }
-        if (!update){
+        if (!update) {
             if (medicalCenter.getStatus() == null || medicalCenter.getStatus().equals("")) {
                 validate.add("Status");
             }
@@ -128,13 +132,15 @@ public class MedicalCenterImpl implements MedicalCenterService {
 
         return validate;
     }
+
     private void associateMedicalCenterWithUser(MedicalCenter medicalCenter, User user) {
         medicalCenter.setUser(user);
         medicalCenterRepository.save(medicalCenter);
     }
-    private boolean validateUserMedicalCenter(User user){
-       List<MedicalCenter> medicalCenters = user.getMedicalCenters();
-       //AQUI SE LE LA VARA
-       return true;
+
+    private boolean validateUserMedicalCenter(User user) {
+        List<MedicalCenter> medicalCenters = user.getMedicalCenters();
+        //AQUI SE LE LA VARA
+        return true;
     }
 }
